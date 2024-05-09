d7c767f367767e421576ad8a3657b0f8f08d20b4
n0v4to <den.litvinov.94@list.ru>
2024-05-09 16:44:13 +0400

    complete class, insert, erase, count, contains, search, print

--- lab_2/lab_2/hash_table.h
+++ lab_2/lab_2/hash_table.h
@@ -38,8 +38,70 @@ public:
 	HashTable& operator=(const HashTable& other) {
 		if (this != other) {
 			_size = other._size;
-			_data = other._data
+			_data = other._data;
 		}
 		return *this;
+	}
+	void print() {
+		for (size_t i = 0; i < _size; ++i) {
+			std::cout << "Bucket " << i << ": ";
+			for (const auto& pair : _data[i]) {
+				std::cout << pair.key << " -> " << pair.value << "; ";
+			}
+			std::cout << std::endl;
+		}
+	}
+	void insert(K key, V value) {
+		size_t index = hash_function(key);
+		_data[index].push_back(Pair(key, value));
+	}
+	void insert_or_assign(K key, V value) {
+		size_t index = hash_function(key);
+		for (auto& pair : _data[index]) {
+			if (pair.key == key) {
+				pair.value = value;
+				return;
+			}
+		}
+		_data[index].push_back(Pair(key, value));
+	}
+	bool contains(V value) {
+		for (const auto& bucket : _data) {
+			for (const auto& pair : bucket) {
+				if (pair.value == value) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+	V* search(K key) {
+		size_t index = hash_function(key);
+		for (auto& pair : _data[index]) {
+			if (pair.key == key) {
+				return &pair.value;
+			}
+		}
+		return nullptr;
+	}
+	bool erase(K key) {
+		size_t index = hash_function(key);
+		for (auto it = _data[index].begin(); it != _data[index].end(); ++it) {
+			if (it->key == key) {
+				_data[index].erase(it);
+				return true;
+			}
+		}
+		return false;
+	}
+	int count(K key) {
+		size_t index = hash_function(key);
+		int count = 0;
+		for (const auto& pair : _data[index]) {
+			if (pair.key == key) {
+				count++;
+			}
+		}
+		return count;
 	}
 };
